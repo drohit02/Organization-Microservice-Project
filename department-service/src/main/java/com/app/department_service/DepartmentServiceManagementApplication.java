d853fa2b6a8ff772daa10009b526130160c0c8b0
drohit02 <drohit045@gmail.com>
2025-03-15 14:10:49 +0530

    department-service initial commit

---
+++ department-service/src/main/java/com/app/department_service/DepartmentServiceManagementApplication.java
@@ -0,0 +1,13 @@
+package com.app.department_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentServiceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartmentServiceManagementApplication.class, args);
+	}
+
+}
---
+++ department-service/src/test/java/com/app/department_service/DepartmentServiceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.department_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
