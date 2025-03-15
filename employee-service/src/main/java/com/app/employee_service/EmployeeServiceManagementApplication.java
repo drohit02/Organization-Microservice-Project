3732ae7ca21835e1edc9abb92d10c9785d223c58
drohit02 <drohit045@gmail.com>
2025-03-15 14:12:13 +0530

    Employee-Service Initial commit

---
+++ employee-service/src/main/java/com/app/employee_service/EmployeeServiceManagementApplication.java
@@ -0,0 +1,13 @@
+package com.app.employee_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeServiceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeServiceManagementApplication.class, args);
+	}
+
+}
